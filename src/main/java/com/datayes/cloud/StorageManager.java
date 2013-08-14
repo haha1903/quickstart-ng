daaebe801f3d6ec84e9abcf794311fc7af81f0ae
hai.chang <>
2013-08-14 05:53:46 +0000

    create volume

--- src/main/java/com/datayes/cloud/StorageManager.java
+++ src/main/java/com/datayes/cloud/StorageManager.java
@@ -22,11 +22,17 @@ public class StorageManager {
 
     public Volume createVolume(Volume volume) throws IOException, InterruptedException {
         Volume result = openstackContext.post("http://10.20.112.226:8776/v1/" + openstackContext.getTenant().getId() + "/volumes", "volume", volume, "volume", Volume.class);
+        Volume status = null;
         for (int i = 0; i < MAX_WAIT; i++) {
-            Volume status = openstackContext.get("http://10.20.112.226:8776/v1/" + openstackContext.getTenant().getId() + "/volumes/" + result.getId(), "volume", Volume.class);
-            if (AVAILABLE.equals(status.getStatus())) break;
+            status = openstackContext.get("http://10.20.112.226:8776/v1/" + openstackContext.getTenant().getId() + "/volumes/" + result.getId(), "volume", Volume.class);
+            if (isAvailable(status)) break;
             Thread.sleep(INTERVAL);
         }
+        if(!isAvailable(status)) throw new IOException("create volume failure, volume = " + volume);
         return result;
+    }
+
+    private boolean isAvailable(Volume status) {
+        return AVAILABLE.equals(status.getStatus());
     }
 }
